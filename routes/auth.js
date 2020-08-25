264ae0df5f6ca0e501759ade897927b69ba2931f
shebinkkabeer <shebinkkabeer@gmail.com>
2020-08-25 21:27:53 +0530

    pre final

--- routes/auth.js
+++ routes/auth.js
@@ -5,10 +5,11 @@ const { signup ,signin,signout,isSignedIn} = require("../controllers/auth");
 const router=express.Router();
 
 router.post("/signup",[
+    body('name',"name should be at least 3 characters").isLength({ min: 3 }),
     body('email',"email is required").isEmail(),
     body('password',"password should be at least 5 characters").isLength({ min: 5 }),
     body('password2',"password mismatch").isLength({ min: 5 }),
-    body('name',"name should be at least 3 characters").isLength({ min: 3 }),
+    
 ],signup)
 
 router.post("/signin",[
